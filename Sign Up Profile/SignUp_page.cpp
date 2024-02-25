379084fcea50738131c7e290652ab564378c8efa
Anila Sahu <99677483+AnilSahu1011@users.noreply.github.com>
2024-02-26 00:10:47 +0530

    Adding Outputs

--- Sign Up Profile/SignUp_page.cpp
+++ Sign Up Profile/SignUp_page.cpp
@@ -8,15 +8,15 @@ int main()
     cout<<"\t\t\t\t----- SIGN UP -----";
     cout<<"\n\tSet your ID Name : ";
     cin>>Id;
-    cout<<"\n\tSet your PassWord Name : ";
+    cout<<"\n\tSet your PassWord : ";
     cin>>Password;         
     
-    cout<<"\n\tYour Id is Created ";
+    cout<<"\n\tYour Id is Created \n";
 
-    cout<<"\t\t\t\t----- LOG IN -----";
+    cout<<"\t\t----- LOG IN -----";
     cout<<"\n\tID Name : ";
     cin>>uId;
-    cout<<"\n\tPassWord Name : ";
+    cout<<"\n\tPassWord : ";
     cin>>uPassword;
     if(Id==uId && Password==uPassword)
     {
@@ -27,15 +27,66 @@ int main()
         cout<<"\n ID is not Available";
         Beep(500,500);
     }
-    else if(Password!=uPassword)
+    else if(Id!=uId && Password!=uPassword)
     {
         cout<<"\nPassword is incorrect";
         Beep(300,500);
     }
     else
     {
-        cout<<"\nYour entered Id and Password both are wrong";
+        cout<<"\nYour entered Id not Available";
         Beep(500,500);
     }
     return 0;
 }
+
+
+// Output
+
+// Case-1
+
+//                               ----- SIGN UP -----
+//         Set your ID Name : Asish#2056
+
+//         Set your PassWord : Anil337
+
+//         Your Id is Created 
+
+//                 ----- LOG IN -----
+//         ID Name : Asish#2056
+
+//         PassWord : Anil337
+
+// Log In successfull
+
+// Case-2
+
+//                               ----- SIGN UP -----
+//         Set your ID Name : Asish#2056
+
+//         Set your PassWord : Anil337
+
+//         Your Id is Created 
+
+//                 ----- LOG IN -----
+//         ID Name : Asish#2056
+
+//         PassWord : Anil346
+
+// Password is incorrect
+
+// Case-3
+
+//                               ----- SIGN UP -----
+//         Set your ID Name : Asish#2056
+
+//         Set your PassWord : Anil337
+
+//         Your Id is Created 
+
+//                 ----- LOG IN -----
+//         ID Name : asish#2056
+
+//         PassWord : Anil346
+
+// ID is not Available
